8d79d4d2b62c693e2751c83aa0217466245ae2c3
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-05-16 21:30:53 -0300

    Incluido botão de login,tela login e tela cadastrar um novo ususario

--- SistemaEscola/Controllers/InfraController.cs
+++ SistemaEscola/Controllers/InfraController.cs
@@ -31,26 +31,93 @@ namespace SistemaEscola.Controllers
        
                 
                 ILogger<InfraController> logger)
-        {
+               {
             _userManager = userManager;
                  _signInManager = signInManager;
                     _logger = logger;
             }
 
-    [HttpGet]
-    [AllowAnonymous]
-    public async Task<IActionResult> Acessar(string returnUrl = null)
-    {
-        await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
-        ViewData["ReturnUrl"] = returnUrl;
-        return View();
-    }
+            [HttpGet]
+            [AllowAnonymous]
+            public async Task<IActionResult> Acessar(string returnUrl = null)
+            {
+                await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+                ViewData["ReturnUrl"] = returnUrl;
+                return View();
+
+            }
+
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult RegistrarNovoUsuario(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegistrarNovoUsuario(RegistrarNovoUsuarioViewModel model, string returnUrl =
+       null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new UsuarioDaAplicacao { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Usuário criou uma nova conta com senha.");
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation("Usuário acesso com a conta criada.");
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);
+            }
+            return View(model);
+
+
+
+        }
+      
+           private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+
+
+       }
+
+        [HttpGet]
+        public async Task<IActionResult> Sair()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("Usuário realizou logout.");
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
     }
+
+
+
+
 }
 
 
---
+++ SistemaEscola/Migrations/20230516235423_cadastro de novos usuários.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaEscola.Migrations
+{
+    public partial class cadastrodenovosusuários : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
